508e5cc1750030b466300af12815876300332d8e
bayevels <bayevels@gmail.com>
2021-10-06 01:26:43 +0000

    Add withdraw_mode to DirectPay service via credit_account method

--- lib/paydunya/direct_pay.rb
+++ lib/paydunya/direct_pay.rb
@@ -1,9 +1,10 @@
 module Paydunya
   class DirectPay < Paydunya::Checkout::Core
-    def credit_account(payee_account, amount)
+    def credit_account(account_alias, amount, withdraw_mode = nil)
       payload = {
-        account_alias: payee_account,
-        amount: amount
+        account_alias: account_alias,
+        amount: amount,
+        withdraw_mode: withdraw_mode
       }
 
       result = send_post_request(Paydunya::Setup.direct_pay_credit_base_url, payload)
